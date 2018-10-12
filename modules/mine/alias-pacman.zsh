cad5910f41539f2d391093b650b59b5dde26798f
Hugh Wang <hghwng@gmail.com>
2018-10-12 13:33:00 +0800

    mine: add pacKv to check kernel versions

--- modules/mine/alias-pacman.zsh
+++ modules/mine/alias-pacman.zsh
@@ -19,3 +19,15 @@ alias makepkg="PKGEXT='.pkg.tar' makepkg"
 pacQlb() {
     pacman -Ql $@ | grep /usr/bin/. | sed -e "s:^\(.*\) /usr/bin/\(.*\):\x1b[92m\1\x1b[0m \2:"
 }
+
+pacKv() {
+    local running=$(uname -r | sed -e 's/-[A-Z]*$//;s/-/./g')
+    local package=$(ls /var/lib/pacman/local/ | grep -E '^linux-[0-9].*' | sed -e 's/linux-//;s/-/./g')
+    if [[ "$running" == "$package" ]]; then
+        echo "Up to date";
+    else
+        echo "Reboot needed";
+    fi;
+    echo "Running: $running"
+    echo "Package: $package"
+}
